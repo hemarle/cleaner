b00e4a345d3c5919649f3290054388eb23b6d79c
Daramola femi <dfmopeoluwa@gmail.com>
2022-01-29 20:22:51 +0100

    Hero component created

--- cleanup/src/Landing.js
+++ cleanup/src/Landing.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Hero from './Hero';
 import './Landing.css'
 function Landing() {
-    return <div className='landing'></div>;
+    return <div className='landing'>
+        <Hero title='Welcome to Khadi
+        Interiors & Cleaning
+        Services'/>
+    </div>;
 }
 
 export default Landing;
