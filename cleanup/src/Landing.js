6fba1d88edec289f211757599d3f4563adc0806c
Daramola femi <dfmopeoluwa@gmail.com>
2022-01-30 14:30:14 +0100

    About mobile css done

--- cleanup/src/Landing.js
+++ cleanup/src/Landing.js
@@ -4,13 +4,16 @@ import './Landing.css'
 
 import broomIcon from './images/broom.svg'
 import heroIcon from './images/hero-bg.png'
+import About from './About';
 function Landing() {
     return <div className='landing'>
         <Hero title='Welcome to Khadi
-                                                                    Interiors & Cleaning
-                                                                    Services' about='Let’s do your dirty work' linkText='Book Now'
+                                                                                            Interiors & Cleaning
+                                                                                            Services' about='Let’s do your dirty work' linkText='Book Now'
             icon={broomIcon}
             image={heroIcon}/>
+        <About summary='We provide execellent services at a friendly
+        budget and we are capable of taking good care of your home every day of the week.'/>
     </div>;
 }
 
