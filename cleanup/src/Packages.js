96a544393df70ffbc0c7f0ebb26fcde6dc69c881
Daramola femi <dfmopeoluwa@gmail.com>
2022-01-30 14:48:58 +0100

    Packages component created

---
+++ cleanup/src/Packages.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Packages.css'
+function Packages() {
+    return <div className='packages'>
+        <div className="packages__Cards"></div>
+    </div>;
+}
+
+export default Packages;
